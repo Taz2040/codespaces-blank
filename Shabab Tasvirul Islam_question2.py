dd4c523009c42e15a7a4ada794d11e0b9c049524
Taz2040 <114352020+Taz2040@users.noreply.github.com>
2024-02-07 15:19:21 +0000

    q3 done reading and importing file to csv

---
+++ Shabab Tasvirul Islam_question2.py
@@ -0,0 +1,30 @@
+
+def main():
+    import pandas as pd
+    print("Initializing...")
+    print("Looking for inventory file(inventory.txt)....")
+    try:
+        
+        # Open the file in read mode
+        with open("inventory.txt", 'r') as file:
+            print("File Found!")
+            print("Reading files...")
+
+            # Define the column names (header)
+            column_names = ['ISBN','Title','Author','Quantity','Price'] 
+            # Read the CSV file into a DataFrame and specify the column names
+            db = pd.read_csv('inventory.txt', names=column_names)
+
+
+            # Display the DataFrame
+            print("File successfully read:")
+
+    except FileNotFoundError:
+        print("Error: File not found.")
+    except pd.errors.EmptyDataError:
+        print("Error: file is empty.")
+    except pd.errors.ParserError as e:
+        print("Error reading file:", e)
+    print("Validating Inventory file format")
+
+main()
--- test-pandas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data.csv')
-#ddd
-#test 3.1
-#3.2
-a=df.to_string()
-print(a)
-
